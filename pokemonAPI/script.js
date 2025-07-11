2287772fdde62a8274e747f1cf02bd355fab34cf
Vassilia <vassilia@gerodimos.com>
2025-07-11 14:55:56 +0200

    worked on the card designs and hover effects

--- pokemonAPI/script.js
+++ pokemonAPI/script.js
@@ -4,7 +4,7 @@
 let pokemonList;
 
 async function onloadFunc() {
-    await loadData("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0");
+    await loadData("https://pokeapi.co/api/v2/pokemon?limit=200&offset=0");
     // console.log(pokemonList);
     renderPokemonList();
 }
@@ -33,15 +33,49 @@ function renderPokemonList() {
     });
 
     contentElement.innerHTML = html;
+
+    activatePopovers()
 } 
 
 function pokemonTemplate(pokemon) {
+    let typesHTML = '';
+    let mainType = pokemon.types[0].type.name;
+    
+    pokemon.types.forEach(typeInfo => {
+        typesHTML += `<img class="${typeInfo.type.name}-type" 
+            src="imgs/icons/${typeInfo.type.name}.svg" 
+            data-bs-toggle="popover" 
+            data-bs-trigger="hover" 
+            data-bs-placement="bottom" 
+            data-bs-content="${typeInfo.type.name}" 
+        /> `;
+    });
+
     return `
-        <div class="card" style="width: 18rem;">
+        <div class="card ${mainType}" style="width: 23rem;">
             <div class="card-body">
-                <h5 class="card-title">#${pokemon.id} ${pokemon.name}</h5>
-                <img src="${pokemon.sprites.other["official-artwork"].front_default}" class="card-img-top" alt="...">
+                <h5 class="card-title title">#${pokemon.id} ${pokemon.name}</h5>
+                <hr>
+                <img src="${pokemon.sprites.other["official-artwork"].front_default}" class="card-img-top pokemon-artwork" alt="...">
+                <hr>
+                <div class="pokemon-types">
+                    ${typesHTML.trim()}
+                </div>
             </div>
         </div>
     `;
 }
+
+function activatePopovers() {
+    let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+      return new bootstrap.Popover(popoverTriggerEl)
+    })
+  });
