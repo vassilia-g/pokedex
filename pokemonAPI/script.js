2e6157b206fbf7caa92b045b6a33f9e67a5af050
Vassilia <vassilia@gerodimos.com>
2025-07-12 22:11:38 +0300

    changes

--- pokemonAPI/script.js
+++ pokemonAPI/script.js
@@ -4,7 +4,8 @@
 let pokemonList;
 
 async function onloadFunc() {
-    await loadData("https://pokeapi.co/api/v2/pokemon?limit=200&offset=0");
+    pokemonList = [];
+    await loadData("https://pokeapi.co/api/v2/pokemon?limit=9&offset=0");
     // console.log(pokemonList);
     renderPokemonList();
 }
@@ -15,7 +16,6 @@ async function loadData(url) {
     let results = responseToJson.results;
 
     // Fetch details for each Pokémon and store in pokemonList
-    pokemonList = [];
     for (const pokemon of results) {
         let detailsResponse = await fetch(pokemon.url);
         let details = await detailsResponse.json();
@@ -97,7 +97,9 @@ function pokemonModalTemplate(pokemon) {
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body ${mainType}">
-                    <img src="${pokemon.sprites.other["official-artwork"].front_default}" class="img-fluid" alt="${pokemon.name}">
+                    <div class="pokemon-modal-img">
+                        <img src="${pokemon.sprites.other["official-artwork"].front_default}" class="img-fluid pokemon-img-fluid" alt="${pokemon.name}">
+                    </div>
                     <ul class="nav nav-tabs" id="myTab-${pokemon.id}" role="tablist">
                         <li class="nav-item" role="presentation">
                             <button class="nav-link active" id="home-tab-${pokemon.id}" data-bs-toggle="tab" data-bs-target="#home-tab-pane-${pokemon.id}" type="button" role="tab" aria-controls="home-tab-pane-${pokemon.id}" aria-selected="true">Main</button>
@@ -109,22 +111,16 @@ function pokemonModalTemplate(pokemon) {
                             <button class="nav-link" id="contact-tab-${pokemon.id}" data-bs-toggle="tab" data-bs-target="#contact-tab-pane-${pokemon.id}" type="button" role="tab" aria-controls="contact-tab-pane-${pokemon.id}" aria-selected="false">Evo Chain</button>
                         </li>
                     </ul>
-                    <div class="tab-content" id="myTabContent-${pokemon.id}">
-                        <div class="tab-pane fade show active container" id="home-tab-pane-${pokemon.id}" role="tabpanel" aria-labelledby="home-tab-${pokemon.id}" tabindex="0">
-                            <strong>Base experience</strong> : ${pokemon.base_experience}
-                            <br>
-                            <strong>Height</strong> : 0.${pokemon.height} m
-                            <br>
-                            <strong>Weight</strong> : ${pokemon.weight} kg
-                            <br>
-                            <strong>Abilities</strong> : ${pokemon.abilities.map(a => a.ability.name).join(', ')}
+                    <div class="tab-content pokemon-tab-content" id="myTabContent-${pokemon.id}">
+                        <div class="container tab-pane fade show active" id="home-tab-pane-${pokemon.id}" role="tabpanel" aria-labelledby="home-tab-${pokemon.id}" tabindex="0">
+                            ${pokemonStatsTemplate(pokemon)}
                         </div>
 
-                        <div class="tab-pane fade" id="profile-tab-pane-${pokemon.id}" role="tabpanel" aria-labelledby="profile-tab-${pokemon.id}" tabindex="0">   
+                        <div class="container tab-pane fade" id="profile-tab-pane-${pokemon.id}" role="tabpanel" aria-labelledby="profile-tab-${pokemon.id}" tabindex="0">   
                             ${progressBars(pokemon, mainType)}
                         </div>
 
-                        <div class="tab-pane fade container" id="contact-tab-pane-${pokemon.id}" role="tabpanel" aria-labelledby="contact-tab-${pokemon.id}" tabindex="0">   
+                        <div class="container tab-pane fade" id="contact-tab-pane-${pokemon.id}" role="tabpanel" aria-labelledby="contact-tab-${pokemon.id}" tabindex="0">   
                             Evo Chain content here
                         </div>
                     </div>
@@ -135,11 +131,51 @@ function pokemonModalTemplate(pokemon) {
 `;
 }
 
+function pokemonStatsTemplate(pokemon) {
+    return `
+        <div class="row">
+            <div class="col-6">
+                Base Experience
+            </div>
+            <div class="col-6">
+                <span class="badge bg-secondary">${pokemon.base_experience}</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                Height
+            </div>
+            <div class="col-6">
+                <span class="badge bg-secondary">${pokemon.height / 10} m</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                Weight
+            </div>
+            <div class="col-6">
+                <span class="badge bg-secondary">${pokemon.weight / 10} kg</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-6">
+                Abilities
+            </div>
+            <div class="col-6">
+                ${pokemon.abilities.map(ability => `
+                    <span class="badge bg-primary" data-bs-toggle="popover" data-bs-trigger="hover" data-bs-placement="bottom" data-bs-html="true" data-bs-content="<span class='ability-text'>${ability.ability.name}</span>">
+                        ${ability.ability.name}
+                    </span>
+                `).join(' ')}
+            </div>
+        </div>
+    `;
+}
 function progressBars(pokemon) {
     let mainType = pokemon.types[0].type.name;
 
     return pokemon.stats.map(stat => `
-        <div class="mb-3 container">
+        <div class="mb-3">
             <strong>${stat.stat.name}:</strong> ${stat.base_stat}
             <div class="progress" role="progressbar" aria-label="${stat.stat.name}" aria-valuenow="${stat.base_stat}" aria-valuemin="0" aria-valuemax="200">
                 <div class="progress-bar ${mainType}-progress" style="width: ${stat.base_stat}%"></div>
