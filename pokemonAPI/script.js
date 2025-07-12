b2c9e2373c2aa8a453fb2d6039a67775acd47dee
Vassilia <vassilia@gerodimos.com>
2025-07-12 17:13:12 +0200

    added information in the modal cards

--- pokemonAPI/script.js
+++ pokemonAPI/script.js
@@ -89,21 +89,63 @@ function pokemonModalTemplate(pokemon) {
     let modalId = `pokemonModal-${pokemon.id}`;
 
     return `
-        <div class="modal fade" id="${modalId}" tabindex="-1" aria-labelledby="modalLabel-${pokemon.id}" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="modalLabel-${pokemon.id}">#${pokemon.id} ${pokemon.name}</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body ${mainType}">
-                        <img src="${pokemon.sprites.other["official-artwork"].front_default}" class="img-fluid" alt="${pokemon.name}">
-                        <!-- Hier kannst du weitere Infos anzeigen -->
+    <div class="modal fade" id="${modalId}" tabindex="-1" aria-labelledby="modalLabel-${pokemon.id}" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="modalLabel-${pokemon.id}">#${pokemon.id} ${pokemon.name}</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body ${mainType}">
+                    <img src="${pokemon.sprites.other["official-artwork"].front_default}" class="img-fluid" alt="${pokemon.name}">
+                    <ul class="nav nav-tabs" id="myTab-${pokemon.id}" role="tablist">
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link active" id="home-tab-${pokemon.id}" data-bs-toggle="tab" data-bs-target="#home-tab-pane-${pokemon.id}" type="button" role="tab" aria-controls="home-tab-pane-${pokemon.id}" aria-selected="true">Main</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="profile-tab-${pokemon.id}" data-bs-toggle="tab" data-bs-target="#profile-tab-pane-${pokemon.id}" type="button" role="tab" aria-controls="profile-tab-pane-${pokemon.id}" aria-selected="false">Stats</button>
+                        </li>
+                        <li class="nav-item" role="presentation">
+                            <button class="nav-link" id="contact-tab-${pokemon.id}" data-bs-toggle="tab" data-bs-target="#contact-tab-pane-${pokemon.id}" type="button" role="tab" aria-controls="contact-tab-pane-${pokemon.id}" aria-selected="false">Evo Chain</button>
+                        </li>
+                    </ul>
+                    <div class="tab-content" id="myTabContent-${pokemon.id}">
+                        <div class="tab-pane fade show active container" id="home-tab-pane-${pokemon.id}" role="tabpanel" aria-labelledby="home-tab-${pokemon.id}" tabindex="0">
+                            <strong>Base experience</strong> : ${pokemon.base_experience}
+                            <br>
+                            <strong>Height</strong> : 0.${pokemon.height} m
+                            <br>
+                            <strong>Weight</strong> : ${pokemon.weight} kg
+                            <br>
+                            <strong>Abilities</strong> : ${pokemon.abilities.map(a => a.ability.name).join(', ')}
+                        </div>
+
+                        <div class="tab-pane fade" id="profile-tab-pane-${pokemon.id}" role="tabpanel" aria-labelledby="profile-tab-${pokemon.id}" tabindex="0">   
+                            ${progressBars(pokemon, mainType)}
+                        </div>
+
+                        <div class="tab-pane fade container" id="contact-tab-pane-${pokemon.id}" role="tabpanel" aria-labelledby="contact-tab-${pokemon.id}" tabindex="0">   
+                            Evo Chain content here
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    `;
+    </div>
+`;
+}
+
+function progressBars(pokemon) {
+    let mainType = pokemon.types[0].type.name;
+
+    return pokemon.stats.map(stat => `
+        <div class="mb-3 container">
+            <strong>${stat.stat.name}:</strong> ${stat.base_stat}
+            <div class="progress" role="progressbar" aria-label="${stat.stat.name}" aria-valuenow="${stat.base_stat}" aria-valuemin="0" aria-valuemax="200">
+                <div class="progress-bar ${mainType}-progress" style="width: ${stat.base_stat}%"></div>
+            </div>
+        </div>
+    `).join('');
 }
 
 function activatePopovers() {
